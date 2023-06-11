c6b7852283239c4267bf0514dbd913362b7c7789
Rıza Berkay AYÇELEBİ <differentiate.function@gmail.com>
2023-06-11 21:21:53 +0300

    proje tamamlandı

--- PROJECTTRACKING/Controllers/GenelBakisController.cs
+++ PROJECTTRACKING/Controllers/GenelBakisController.cs
@@ -14,10 +14,136 @@ namespace PROJECTTRACKING.Controllers
         public ActionResult Index()
         {
             int projeSayisi = db.personelProjeleris.Count();
-            ViewBag.projesayisi = projeSayisi;
-            int tamamlananProje = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == true).Count();
-            ViewBag.tamamlananProje = tamamlananProje;
+            ViewBag.ProjeSayisi = projeSayisi;
+
+
+
+            int tamamlanmisProje = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == true).Count();
+            ViewBag.TamamlanmisProje = tamamlanmisProje;
+
+
+            var yuksekOncelikliProjeler = db.personelProjeleris.Where(p => p.OncelikDurumu == "Yüksek Öncelikli").Count();
+            ViewBag.YuksekOncelikli = yuksekOncelikliProjeler;
+
+            var dusukOncelikliProjeler = db.personelProjeleris.Where(p => p.OncelikDurumu == "Düşük Öncelikli").Count();
+            ViewBag.DusukOncelikli = dusukOncelikliProjeler;
+
+
+            var ortaOncelikliProjeler = db.personelProjeleris.Where(p => p.OncelikDurumu == "Orta Öncelikli").Count();
+            ViewBag.OrtaOncelikli = ortaOncelikliProjeler;
+
+            var basariliveyuksek = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == true && p.OncelikDurumu == "Yüksek Öncelikli").Count();
+            ViewBag.YuksekVeBasarili = basariliveyuksek;
+
+            var basariliveorta = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == true && p.OncelikDurumu == "Orta Öncelikli").Count();
+            ViewBag.OrtaVeBasarili = basariliveorta;
+
+
+            var basarilivedusuk = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == true && p.OncelikDurumu == "Düşük Öncelikli").Count();
+            ViewBag.DusukVeBasarili = basarilivedusuk;
+
+
+
+            int tamamlanmamisProje = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == false).Count();
+            ViewBag.TamamlanmamisProje = tamamlanmamisProje;
+
+
+            var personelProjeListesi = db.personelProjeleris.ToList();
+            var personelTamamlanmisProjeSayisi = new Dictionary<int, int>(); 
+            foreach (var personel in db.PersonelBilgileris.ToList())
+            {
+                int tamamlanmisProjeSayisi = 0;
+                foreach (var proje in personel.PersonelProjeleris)
+                {
+                    if (proje.TamamlanmaDurumu == true)
+                    {
+                        tamamlanmisProjeSayisi++;
+                    }
+                }
+                personelTamamlanmisProjeSayisi[personel.PersonelBilgileriId] = tamamlanmisProjeSayisi;
+            }
+
+            var siraliPersoneListesi = personelTamamlanmisProjeSayisi.OrderByDescending(x => x.Value); 
+            var enCokTamamlananPersonelId = siraliPersoneListesi.First().Key; 
+            var enCokTamamlananPersonel = db.PersonelBilgileris.FirstOrDefault(p => p.PersonelBilgileriId == enCokTamamlananPersonelId);
+            ViewBag.EnCokTamamlayanPersonelBilgisi = enCokTamamlananPersonel.AdSoyad;
+
+
+
+            int enCokProjeTamamlayanPersonelProjeSayisi = personelTamamlanmisProjeSayisi[enCokTamamlananPersonelId];
+            ViewBag.EnCokProjeTamamlayanPersonelinProjeSayisi = enCokProjeTamamlayanPersonelProjeSayisi;
             return View();
         }
+
+
+        public ActionResult Genelİstatistik()
+        {
+
+            var personeller = db.PersonelBilgileris.ToList();
+            var personelProjeleri = db.personelProjeleris.ToList();
+            var tamamlananProjeSayisi = new Dictionary<int, int>();
+            var tamamlanmayanProjeSayisi = new Dictionary<int, int>();
+            var toplamProjeSayisi = new Dictionary<int, int>();
+            foreach (var personel in personeller)
+            {
+                int tamamlananProje = 0;
+                int tamamlanmayanProje = 0;
+                int toplamProje = 0;
+                foreach (var proje in personelProjeleri)
+                {
+                    if (proje.PersonelBilgileris.Contains(personel))
+                    {
+
+                        toplamProje++;
+                        if (proje.TamamlanmaDurumu)
+                        {
+                            tamamlananProje++;
+                        }
+                        else
+                        {
+                            tamamlanmayanProje++;
+                        }
+                    }
+                }
+                tamamlananProjeSayisi[personel.PersonelBilgileriId] = tamamlananProje;
+                tamamlanmayanProjeSayisi[personel.PersonelBilgileriId] = tamamlanmayanProje;
+                toplamProjeSayisi[personel.PersonelBilgileriId] = toplamProje;
+
+
+            }
+
+            ViewBag.TamamlananProjeSayisi = tamamlananProjeSayisi;
+            ViewBag.TamamlanmayanProjeSayisi = tamamlanmayanProjeSayisi;
+            ViewBag.ToplamProjeSayisi = toplamProjeSayisi;
+
+
+
+            int projeSayisi = db.personelProjeleris.Count();
+            ViewBag.ProjeSayisi = projeSayisi;
+
+
+
+            int personelSayisi = db.PersonelBilgileris.Count();
+            ViewBag.PersonelSayisi = personelSayisi;
+
+
+            int tamamlanmisProje = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == true).Count();
+            ViewBag.TamamlanmisProje = tamamlanmisProje;
+
+            int tamamlanmamisProje = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == false).Count();
+            ViewBag.TamamlanmamisProje = tamamlanmamisProje;
+
+
+
+            var basarisizveyuksek = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == false && p.OncelikDurumu == "Yüksek Öncelikli").Count();
+            ViewBag.YuksekVeBasarisiz = basarisizveyuksek;
+
+            var basarisizveorta = db.personelProjeleris.Where(p => p.TamamlanmaDurumu == false && p.OncelikDurumu == "Orta Öncelikli").Count();
+            ViewBag.OrtaVeBasarisiz = basarisizveorta;
+
+
+            return View(personeller);
+        }
+
     }
 }
